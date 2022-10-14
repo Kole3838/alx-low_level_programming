7dfa647b14317f385343f24a3ffee474434e5e70
kole samuel <kolesamuel55@yahoo.com.com>
2022-10-13 21:04:49 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- Serves as the entry point for the program
+ * Return: By default returns zero if no erros otherwise returns non-zero value
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
