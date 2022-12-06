02c649b513de51a43984898acfcb4443ce1c7dd5
kole samuel <kolesamuel55@yahoo.com.com>
2022-12-05 19:51:56 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,8 +25,9 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
-
-		rwr = write(fd, text_content, nletters);
+		;
+	
+	rwr = write(fd, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);
